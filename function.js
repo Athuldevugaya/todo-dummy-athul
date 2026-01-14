56769dc1a4411f04edb31a07dc3e04d49c43c30b
Athuldevugaya <athulsivanadg@gmail.com>
2026-01-14 11:20:14 +0530

    added file
    added js and html file

---
+++ function.js
@@ -0,0 +1,21 @@
+function test()
+{
+    console.log("test is working")
+}test()
+console.log("normal flow");
+function greet(name)
+{
+    console.log("good morining",name);
+    return "success"
+    }
+greet('arun')
+function multi(num1,num2)
+{
+    return num1*num2
+}
+let res= multi(5,6)
+console.log("result=",res);
+function findpow(m,n)
+{
+    console.log("power is ",m,n);
+}findpow(3,4);
